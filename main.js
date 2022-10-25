efcf4ca387d6b2b41c0ce24d777f0278cb0746b5
joaopedro-pavani <99080735+folivorasloth@users.noreply.github.com>
2022-10-24 23:31:42 -0300

    Add Features

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const menuBar = document.querySelector(".menuBar");
 const navList = document.querySelector(".navList");
+const iconProfile = document.querySelector(".iconProfile");
+const profileList = document.querySelector(".profileList");
 
 menuBar.addEventListener("click", () => {
     menuBar.classList.toggle("active");
@@ -10,3 +12,8 @@ document.querySelectorAll(".navItems").forEach (n => n.addEventListener("click",
     menuBar.classList.remove("active");
     navList.classList.remove("active");
 }))
+
+iconProfile.addEventListener("click", () => {
+    iconProfile.classList.toggle("active");
+    profileList.classList.toggle("active");
+})
